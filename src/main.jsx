4faeebf814c60fea509bc14363f1839b6d6be2fe
Balaraman-Dev <balaramann2004@gmail.com>
2025-12-07 03:10:15 +0530

    update certificate

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import App from './App.jsx'
-import '../app/css/globals.scss'
-import '../app/css/card.scss'
+import '@/app/css/card.scss'
+import '@/app/css/globals.scss'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
